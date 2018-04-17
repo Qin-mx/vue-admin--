a3398a9cd59dd449eba650f1ae3ed9e9ae58886b
qin-mx <qmx_914@163.com>
2018-04-17 15:48:40 +0800

    第一次上传

--- src/mock/index.js
+++
@@ -1,9 +0,0 @@
-import Mock from 'mockjs'
-import loginAPI from './login'
-
-// 登录相关
-Mock.mock(/\/login\/login/, 'post', loginAPI.loginByUsername)
-Mock.mock(/\/login\/logout/, 'post', loginAPI.logout)
-Mock.mock(/\/user\/info\.*/, 'get', loginAPI.getUserInfo)
-
-export default Mock
--- src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  status: state => state.user.status,
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name,
-  roles: state => state.user.roles
-}
-
-export default getters
